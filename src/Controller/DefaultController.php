431152f8789b60f5293e63ee2b3394a0dbd938d4
Rachid Boudjenane <boudj@Rachids-MacBook-Pro.local>
2020-12-02 14:48:32 +0100

    make:user done

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="home")
