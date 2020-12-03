594542e706ecab1e6b94f86b6261d116cf4a15bf
Rachid Boudjenane <boudj@Rachids-MacBook-Pro.local>
2020-12-03 16:12:31 +0100

    form contact updade

--- src/Form/AdressType.php
+++ src/Form/AdressType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Adress;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +13,23 @@ class AdressType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('adressLine1')
-            ->add('adressLine2')
-            ->add('zipcode')
-            ->add('city')
-            ->add('contact')
+            ->add('adressLine1', TextType::class, [
+                'label' => "Adresse",
+                'attr' => [ 'placeholder' => "Saisissez l'adresse"]
+            ])
+            ->add('adressLine2', TextType::class, [
+                'label' => "Complément d'adresse",
+                'attr' => [ 'placeholder' => "Saisissez le complément d'adresse"]
+            ])
+            ->add('zipcode', TextType::class, [
+                'label' => "Code Postal",
+                'attr' => [ 'placeholder' => 'Saisissez le code postal']
+            ])
+            ->add('city', TextType::class, [
+                'label' => "Ville",
+                'attr' => [ 'placeholder' => 'Saisissez la ville']
+            ])
+      
         ;
     }
 
