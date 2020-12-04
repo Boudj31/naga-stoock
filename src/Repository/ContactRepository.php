80f0ba772400085303925559239d5f9fb02e3371
Ansaka Mahamoudou <anssaka.mahamoudou@icloud.com>
2020-12-04 13:54:33 +0100

    Ajouts pagination

--- src/Repository/ContactRepository.php
+++ src/Repository/ContactRepository.php
@@ -4,6 +4,8 @@ namespace App\Repository;
 
 use App\Entity\Contact;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
+
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +19,13 @@ class ContactRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Contact::class);
+    }
+
+    public function findAllPagination() : Query
+    {
+        return $this->createQueryBuilder('c')
+                ->orderBy('c.id', 'DESC')
+                ->getQuery();
     }
 
     // /**
